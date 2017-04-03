d4eb172deb8f8578678228f8aac2ea3e2128559b
Rory McElearney <rorymcelearney@gmail.com>
2017-04-03 23:56:19 +0300

    added partial branch

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const staticHandler = require('./static.js');
 const homeHandler = require('./home.js');
-const reverseHandler = require('./reverse.js');
 
 module.exports = [
-  staticHandler, homeHandler, reverseHandler
+  staticHandler, homeHandler
 ]
